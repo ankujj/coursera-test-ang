0e5c0b09cfb9d5cf593fcaa117869df70538e525
ankujj <mail2ankuj@gmail.com>
2018-06-04 22:36:55 -0400

    Second Assignment

---
+++ assignment-2/app.js
@@ -0,0 +1,119 @@
+/* defining the IFFE below */
+(function() {
+
+	'use strict';
+
+	angular.module('ShoppingListApp',[])
+	.controller('ShoppingListController',ShoppingListController)
+	.controller('ShoppedListController',ShoppedListController)
+	.provider('ShoppingListService',ShoppingListServiceProvider)
+	.provider('ShoppedListService',ShoppingListServiceProvider)
+	.config(ShoppingListConfig);
+
+	var itemList = [
+					{name: "Soda",
+					quantity: "10"
+					},
+					{name: "Apples",
+					quantity: "12"
+					},
+					{name: "Mangoes",
+					quantity: "6"
+					},
+					{name:"Maggi Noodles",
+					quantity: "10"
+					},
+					{name:"popcorn",
+					quantity:"2"
+					},
+					{name:"Body wash",
+					quantity:"1"
+					}
+	];
+	
+
+	ShoppingListConfig.$inject = ['ShoppingListServiceProvider'];
+
+	function ShoppingListConfig(ShoppingListServiceProvider){
+		
+		ShoppingListServiceProvider.defaults.itemList = itemList;
+	}
+	
+		
+
+	ShoppingListController.$inject = ['ShoppingListService','ShoppedListService'];
+
+	function ShoppingListController(ShoppingListService,ShoppedListService){
+
+		var shoppingList = this;
+
+		shoppingList.items = ShoppingListService.getItems();
+
+		shoppingList.buyItem = function(itemIndex){
+			
+			ShoppedListService.addItem(ShoppingListService.getItemAtIndex(itemIndex));
+
+		};
+
+		shoppingList.add = function(){
+
+			ShoppingListService.addItem({name:shoppingList.newItem,
+										 quantity:shoppingList.newQuantity
+										}
+									   );
+		}
+
+		
+	}
+
+	ShoppedListController.$inject = ['ShoppedListService'];
+	
+	function ShoppedListController(ShoppedListService){
+		
+		var shoppedList = this;
+		shoppedList.items = ShoppedListService.getItems();
+		
+	}
+
+	function ShoppingListServiceProvider(){
+
+		this.defaults = {
+			itemList:[]
+		}		
+
+		this.$get = function(){
+
+			return new ShoppingListService(this.defaults.itemList);
+		};		
+
+	}
+
+	function ShoppingListService(itemList){
+
+		var items = itemList || [];
+		
+		this.addItem = function(item){
+			items.push(item);
+		};
+
+		this.getItemAtIndex = function(itemIndex){
+			return items[itemIndex];
+		}
+
+		this.removeItem = function(itemIndex){
+			items.splice(itemIndex,1);
+		}
+
+		this.getItems = function(){
+
+			return items;
+		}
+
+		
+		
+	}
+
+	
+
+	
+})();
